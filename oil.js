6880b9143f868380fdc446fc585a142157293e85
roshni-2003 <56320368+roshni-2003@users.noreply.github.com>
2020-12-05 21:49:16 -0500

    Update oil.js

--- oil.js
+++ oil.js
@@ -1,26 +1,38 @@
-// Get the elements with class="column"
-var elements = document.getElementsByClassName("column");
+let elements = document.getElementsByClassName("column");
 
-// Declare a "loop" variable
-var x;
+// Declare a loop variable
+let i;
 
 // Full-width images
 function one() {
-    for (x = 0; x < elements.length; x++) {
-        elements[x].style.flex = "100%";
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "100%";  // IE10
+        elements[i].style.flex = "100%";
     }
 }
 
 // Two images side by side
 function two() {
-    for (x = 0; x < elements.length; x++) {
-        elements[x].style.flex = "50%";
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "50%";  // IE10
+        elements[i].style.flex = "50%";
     }
 }
 
 // Four images side by side
 function four() {
-    for (x = 0; x < elements.length; x++) {
-        elements[x].style.flex = "25%";
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "25%";  // IE10
+        elements[i].style.flex = "25%";
     }
 }
+
+// Add active class to the current button (highlight it)
+let header = document.getElementById("myHeader");
+let btns = header.getElementsByClassName("btn");
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    })}
